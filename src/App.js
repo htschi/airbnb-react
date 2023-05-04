eaba33bddaed8d5433d554b37d05f5184e6c52c0
htschi <125224273+htschi@users.noreply.github.com>
2023-05-04 12:14:53 +0700

    Creating Routes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import SignUp from './pages/signUp'
 import Profile from './pages/profile'
 import Login from './pages/login'
 import HouseEdit from './pages/houseEdit'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 
 
 
 function App() {
-  return <House />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/house' element={<House />} />
+        <Route path='/housecreate' element={<HouseCreate />} />
+        <Route path='/' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/houseedit' element={<HouseEdit />} />
+        <Route path='/houses' element={<Houses />} />
+        <Route path='/housecreate' element={<HouseCreate />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
